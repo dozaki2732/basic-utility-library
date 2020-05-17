342b4b7878f13e3763e34287371d2c106be1567f
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-05-16 20:46:59 -0700

    add and subtract functions

--- functions.js
+++ functions.js
@@ -4,3 +4,8 @@ function add(input1, input2) {
    // R: a single JavaScript value
    return input1 + input2;
 }
+
+function subtract(subtract1, subtract2) {
+   // R: subtracting the first v fromt he second v
+   return subtract2 - subtract1;
+}
